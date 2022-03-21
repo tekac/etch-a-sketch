9f7c7c96a46fd6ac4df35465fd23f7a68da0607c
tekac <soehnerj@gmail.com>
2022-03-21 08:59:21 -0400

    finished mousedown/mouseup logic

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container");
+let interval_;
+let isDrawing;
 
 const makeRows = (rows, cols) => {
   container.style.setProperty("--grid-rows", rows);
@@ -7,8 +9,20 @@ const makeRows = (rows, cols) => {
   for (let c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
+    cell.style.height = 600 / `(${rows} * ${cols})`;
+
+    cell.addEventListener("mousedown", () => {
+      isDrawing = true;
+    });
     cell.addEventListener("mousemove", () => {
-      cell.style.backgroundColor = "black";
+      if (isDrawing === true) {
+        cell.style.backgroundColor = "black";
+      }
+    });
+    cell.addEventListener("mouseup", () => {
+      if (isDrawing === true) {
+        isDrawing = false;
+      }
     });
   }
 };
