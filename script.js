b9bc554d070306dbf2a64b4665fdb046f87b19b3
tekac <soehnerj@gmail.com>
2022-03-21 14:53:35 -0400

    finished slider logic, now doing random RGB

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector(".container");
-let interval_;
+const clearBtn = document.querySelector(".clear-btn");
+const sliderValue = document.querySelector(".slider-value");
+const slider = document.querySelector(".slider");
 let isDrawing;
+sliderValue.textContent = slider.value;
 
 const makeRows = (rows, cols) => {
   container.style.setProperty("--grid-rows", rows);
@@ -26,5 +29,34 @@ const makeRows = (rows, cols) => {
     });
   }
 };
+
+const removeAllChildNodes = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+};
+
+slider.addEventListener("input", () => {
+  let val = slider.value;
+  sliderValue.textContent = val;
+  removeAllChildNodes(container);
+  makeRows(val, val);
+});
+
+const randColor = () => {
+  let arr = [];
+
+  for (let i = 0; i < 3; i++) {
+    let random = Math.floor(Math.random() * 255);
+    arr.push(random);
+  }
+  console.log(arr);
+};
+
+const clearGrid = () => {
+  const gridItems = document.querySelectorAll(".grid-item");
+  gridItems.forEach((item) => (item.style.backgroundColor = ""));
+};
+clearBtn.addEventListener("click", clearGrid);
 
 makeRows(16, 16);
