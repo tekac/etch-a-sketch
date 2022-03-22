d9c9996a5e4112828bf03ec896c84c16de6a9818
tekac <soehnerj@gmail.com>
2022-03-21 21:37:14 -0400

    finished project, began attempts to do a shading

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ const container = document.querySelector(".container");
 const clearBtn = document.querySelector(".clear-btn");
 const sliderValue = document.querySelector(".slider-value");
 const slider = document.querySelector(".slider");
+const rgbBtn = document.querySelector(".rgb-btn");
+const shadeBtn = document.querySelector(".shade-btn");
 let isDrawing;
 sliderValue.textContent = slider.value;
 
+// RGB Controls
+rgbBtn.addEventListener("click", () => {
+  rgbBtn.classList.toggle("rgb-on");
+});
+
+// Shader Controls
+shadeBtn.addEventListener("click", () => {
+  shadeBtn.classList.toggle("shade-on");
+});
+
+// CREATE GRID
 const makeRows = (rows, cols) => {
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
@@ -19,7 +32,12 @@ const makeRows = (rows, cols) => {
     });
     cell.addEventListener("mousemove", () => {
       if (isDrawing === true) {
-        cell.style.backgroundColor = "black";
+        if (rgbBtn.classList.contains("rgb-on")) {
+          let ran = randColor();
+          cell.style.backgroundColor = `rgb(${ran[0]},${ran[1]},${ran[2]})`;
+        } else {
+          cell.style.backgroundColor = "black";
+        }
       }
     });
     cell.addEventListener("mouseup", () => {
@@ -50,7 +68,24 @@ const randColor = () => {
     let random = Math.floor(Math.random() * 255);
     arr.push(random);
   }
-  console.log(arr);
+  return arr;
+};
+
+const LightenColor = function (color, percent) {
+  var num = parseInt(color, 16),
+    amt = Math.round(2.55 * percent),
+    R = (num >> 16) + amt,
+    B = ((num >> 8) & 0x00ff) + amt,
+    G = (num & 0x0000ff) + amt;
+
+  return (
+    0x1000000 +
+    (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
+    (B < 255 ? (B < 1 ? 0 : B) : 255) * 0x100 +
+    (G < 255 ? (G < 1 ? 0 : G) : 255)
+  )
+    .toString(16)
+    .slice(1);
 };
 
 const clearGrid = () => {
